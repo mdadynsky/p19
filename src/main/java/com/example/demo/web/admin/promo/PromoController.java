5d3fe0bd16513852042e12932f1c02830b5328d2
BuildTools <unconfigured@null.spigotmc.org>
2022-03-17 11:38:30 +0700

    Создана таблица данных о продуктах в промо-блоке.

--- src/main/java/com/example/demo/web/admin/promo/PromoController.java
+++ src/main/java/com/example/demo/web/admin/promo/PromoController.java
@@ -1,11 +1,18 @@
 package com.example.demo.web.admin.promo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Автор: Мжачих Юрий
- * Контроллер где будут редактироватся блок промо акций
+ * Контроллер где будет редактироваться блок промо акций
  */
+
+@Controller
 public class PromoController {
-    /**
-     * commit test
-     */
+
+    @GetMapping("/admin/promo/promo")
+    public String showPromoList() {
+        return "admin/promo/promo";
+    }
 }
