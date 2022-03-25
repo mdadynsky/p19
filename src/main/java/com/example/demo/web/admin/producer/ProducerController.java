dc234e4d6ba42cd1bdf3b8d6f673acc19e4e3465
Evgeny Kopylov <zhenya.ko1711@gmail.com>
2022-03-25 22:13:10 +0700

    add producers page and controller

--- src/main/java/com/example/demo/web/admin/producer/ProducerController.java
+++ src/main/java/com/example/demo/web/admin/producer/ProducerController.java
@@ -1,8 +1,18 @@
 package com.example.demo.web.admin.producer;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Автор: Евгений Копылов
  * Список производителей
  */
+@Controller
 public class ProducerController {
+
+    @GetMapping("/admin/producer/producers")
+    public String showProducerList() {
+        return "admin/producer/producers";
+    }
+
 }
---
+++ src/main/resources/templates/admin/producer/producers.ftl
@@ -0,0 +1,15 @@
+<#import "/spring.ftl" as spring/>
+<#import "../../theme/layout.ftl" as layout/>
+
+<@layout.layout>
+    <h1>Список производителей</h1>
+
+    <table class="table">
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Название</th>
+            <th scope="col">Страна</th>
+            <th scope="col">Адрес</th>
+        </tr>
+    </table>
+</@layout.layout>
