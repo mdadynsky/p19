553926286df3f7f44203b90c11e72b418f3599a3
Evgeny Kopylov <zhenya.ko1711@gmail.com>
2022-03-25 22:26:00 +0700

    add producer model

---
+++ src/main/java/com/example/demo/model/Producer.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+public class Producer {
+    private Integer id;
+    private String producerName;
+    private String producerCountry;
+
+    public Integer getId() { return id; }
+
+    public void setId(Integer id) { this.id = id; }
+
+    public String getProducerName() { return producerName; }
+
+    public void setProducerName(String producerName) { this.producerName = producerName; }
+
+    public String getProducerCountry() { return producerCountry; }
+
+    public void setProducerCountry(String producerCountry) { this.producerCountry = producerCountry; }
+}
