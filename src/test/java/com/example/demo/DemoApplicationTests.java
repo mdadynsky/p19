5b75d4251323a36aba4fa8910919a0977ff37e2e
Maxim Dadynsky <mdadynsky@nocodeams.com>
2022-04-21 10:56:16 +0700

    Модульное тестирование

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import com.example.demo.service.UserService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private UserService userService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testAdd() {
+		assertEquals("Проверка суммы",42, Integer.sum(19, 23));
+	}
+
+	@Test
+	public void userList() {
+		List user = userService.getUsers();
+		assertEquals("Пользователей должно быть 6", 6, user.size());
 	}
 
 }
