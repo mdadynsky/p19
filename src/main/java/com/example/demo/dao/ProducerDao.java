92e7b1813697346b80de5931192e1b683212e520
Evgeny Kopylov <zhenya.ko1711@gmail.com>
2022-04-22 22:59:39 +0700

    add some gets, delete, update and insert to ProducerDao

--- src/main/java/com/example/demo/dao/ProducerDao.java
+++ src/main/java/com/example/demo/dao/ProducerDao.java
@@ -22,4 +22,51 @@ public class ProducerDao extends BaseDao {
     public List<Producer> getProducerList() {
         return jdbcTemplate.query("select * from producer", new ProducerRowMapper());
     }
+
+    public Producer getProducerById(Integer producerId){
+        List<Producer> producers = jdbcTemplate.query(
+                "select * from producer where id = ?",
+                preparedStatement -> preparedStatement.setInt(1, producerId),
+                new ProducerRowMapper());
+
+        if (producers.isEmpty())
+            return null;
+
+        return producers.get(0);
+    }
+
+    public Producer getProducerByName(String producerName){
+        List<Producer> producers = jdbcTemplate.query(
+                "select * from producer where producerName = ?",
+                preparedStatement -> preparedStatement.setString(1, producerName),
+                new ProducerRowMapper());
+
+        if (producers.isEmpty())
+            return null;
+
+        return producers.get(0);
+    }
+
+    public void deleteProducer(Integer producerId) {
+        jdbcTemplate.update("delete from producer where id = " + producerId);
+    }
+
+    public void update(Producer producer) {
+        jdbcTemplate.update(
+                "update producer set producerName = ?, producerCountry = ? where id = ?",
+                preparedStatement -> {
+                    preparedStatement.setString(1, producer.getProducerName());
+                    preparedStatement.setString(2, producer.getProducerCountry());
+                    preparedStatement.setInt(3, producer.getId());
+                });
+    }
+
+    public void insert(Producer producer) {
+        jdbcTemplate.update(
+                "insert into producer (producerName, producerCountry) values (?, ?)",
+                preparedStatement -> {
+                    preparedStatement.setString(1, producer.getProducerName());
+                    preparedStatement.setString(2, producer.getProducerCountry());
+                });
+    }
 }
